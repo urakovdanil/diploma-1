3a2371ca6df015390fee05d3a524424dd37fb116
Daniil Urakov <urakov.danil@yandex.ru>
2024-05-01 12:52:12 +0500

    Auth test fix

--- internal/api/auth/auth.go
+++ internal/api/auth/auth.go
@@ -96,7 +96,6 @@ func (a *Auth) RegisterHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "something went wrong", http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
 
 	token, err := generateToken(config.Applied, user)
 	if err != nil {
@@ -104,6 +103,7 @@ func (a *Auth) RegisterHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Authorization", "Bearer "+token)
+	w.WriteHeader(http.StatusOK)
 }
 
 func (a *Auth) LoginHandlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -139,4 +139,5 @@ func (a *Auth) LoginHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Authorization", "Bearer "+token)
+	w.WriteHeader(http.StatusOK)
 }
